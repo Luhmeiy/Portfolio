e07ac5ca2ef7f5d0cb6ef2fea75f65b1f35db415
Luhmeiy <Medeiros.luizhenrique@yahoo.com.br>
2023-02-03 21:54:52 -0300

    Update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,33 @@
 // libraries
-import { List, X } from "phosphor-react";
-import { motion } from "framer-motion";
+import {
+	Desktop,
+	House,
+	PaperPlaneRight,
+	Scroll,
+	SquaresFour,
+	User,
+	X,
+} from "phosphor-react";
+import { AnimatePresence, motion } from "framer-motion";
 
 // React
 import { useState } from "react";
+
+const icons = [
+	<House size={24} />,
+	<User size={24} />,
+	<Scroll size={24} />,
+	<Desktop size={24} />,
+	<PaperPlaneRight size={24} />,
+];
 
 const Navbar = () => {
 	const [toggle, setToggle] = useState(false);
 
 	return (
-		<nav className="w-full flex justify-center px-4 py-8 bg-[rgba(255, 255, 255, .25)] backdrop-filter backdrop-blur-sm border-b border-b-[rgba(255, 255, 255, .18)] fixed z-10">
-			<div className="w-full max-w-6xl flex justify-between items-center ">
-				<div className="text-3xl font-bold text-secondary">
+		<nav className="w-full flex justify-center px-4 py-8 bg-[rgba(255, 255, 255, .25)] backdrop-filter backdrop-blur-sm border-b border-b-[rgba(255, 255, 255, .18)] fixed z-10 max-tablet:bottom-0 max-tablet:bg-white max-tablet:px-6 max-tablet:py-3 max-tablet:rounded-xl max-tablet:shadow-[0_0_20px_rgba(168,168,168,.15)]">
+			<div className="w-full max-w-6xl flex justify-between items-center">
+				<div className="text-3xl font-bold text-secondary max-tablet:text-2xl">
 					Medeiros
 				</div>
 
@@ -20,9 +36,8 @@ const Navbar = () => {
 						(item) => (
 							<li
 								key={`link-${item}`}
-								className="mx-4 cursor-pointer flex flex-col items-center group"
+								className="mx-4 cursor-pointer flex justify-center"
 							>
-								<div className="w-2 h-2 rounded-full mb-1 duration-[.3s] ease-in-out group-hover:bg-secondary" />
 								<a
 									href={`#${item}`}
 									className="text-gray-color uppercase font-bold duration-[.3s] ease-in-out hover:text-secondary"
@@ -34,48 +49,54 @@ const Navbar = () => {
 					)}
 				</ul>
 
-				<div className="w-9 h-9 rounded-full relative flex justify-center items-center bg-secondary tablet:hidden">
-					<List
-						onClick={() => setToggle(true)}
-						color="white"
-						className="w-[70%] h-[70%] cursor-pointer"
-					/>
+				<div className="rounded-full relative flex justify-center items-center tablet:hidden">
+					<SquaresFour onClick={() => setToggle(true)} size={26} />
 
-					{toggle && (
-						<motion.div
-							whileInView={{ x: [300, 0] }}
-							transition={{ duration: 0.85, ease: "easeOut" }}
-							className="h-screen bg-white fixed z-20 p-4 inset-y-0 right-0 w-[80%] flex flex-col justify-end items-end shadow-[0_0_20px_rgba(168,168,168,.15)]"
-						>
-							<X
-								onClick={() => setToggle(false)}
-								size={35}
-								className="text-seconday-color my-2 mx-4 cursor-pointer"
-							/>
-
-							<ul className="w-full h-full flex flex-col justify-start items-start">
-								{[
-									"home",
-									"about",
-									"skills",
-									"work",
-									"contact",
-								].map((item) => (
-									<li
-										key={item}
-										className="m-4 cursor-pointer flex flex-col items-center group"
-									>
-										<a
-											href={`#${item}`}
-											className="text-gray-color uppercase text-sm font-bold duration-[.3s] ease-in-out hover:text-secondary"
+					<AnimatePresence>
+						{toggle && (
+							<motion.div
+								initial={{ bottom: -200 }}
+								animate={{ bottom: 0 }}
+								exit={{ bottom: -200 }}
+								transition={{
+									duration: 0.5,
+									ease: "easeInOut",
+								}}
+								className="bg-white fixed z-20 px-6 py-4 inset-x-0 inset-b-0 w-full flex flex-col items-end rounded-3xl shadow-[0_0_20px_rgba(168,168,168,.15)]"
+							>
+								<ul className="w-full h-full grid grid-cols-3">
+									{[
+										"home",
+										"about",
+										"skills",
+										"work",
+										"contact",
+									].map((item, i) => (
+										<li
+											key={item}
+											className="m-4 cursor-pointer flex flex-col items-center group"
 										>
-											{item}
-										</a>
-									</li>
-								))}
-							</ul>
-						</motion.div>
-					)}
+											<a
+												href={`#${item}`}
+												className="text-gray-color uppercase text-sm font-bold flex flex-col items-center duration-[.3s] ease-in-out hover:text-secondary"
+											>
+												{icons[i]}
+												<span className="mt-1">
+													{item}
+												</span>
+											</a>
+										</li>
+									))}
+								</ul>
+								<X
+									onClick={() => setToggle(false)}
+									size={24}
+									weight={"bold"}
+									className="text-red-400"
+								/>
+							</motion.div>
+						)}
+					</AnimatePresence>
 				</div>
 			</div>
 		</nav>
